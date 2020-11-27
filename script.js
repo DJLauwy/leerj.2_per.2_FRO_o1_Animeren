a26926bbce7d061857978bddb2474999ef0f8265
DJLauwy <laudehoop@hotmail.com>
2020-11-27 13:46:46 +0100

    video 5 afgerond
    start met video 6 begonnen

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 document.querySelector('.animeer-knop').addEventListener('click', (e) => {
 	document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
 });
+
+
+//na enige tijd na het laden van de pagina start de animatie van het vlakje vanzelf
+setTimeout(()=>{
+	document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
+}, 2000);
