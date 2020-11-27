94367feb065ae19ed412d7939d5558002e601db5
DJLauwy <laudehoop@hotmail.com>
2020-11-27 13:08:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# leerj.2_per.2_FRO_o1_Animeren
+ Leerjaar 2 Periode 2 FRO opdracht1 Animeren | Lau de Hoop MD2Ab
---
+++ script.js
@@ -0,0 +1,4 @@
+//klink op animeer-knop laat vlakje animeren door class toe te voegen of weg te nemen
+document.querySelector('.animeer-knop').addEventListener('click', (e) => {
+	document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
+});
