bdeac374b265ee2fc705eb6ecf11097055b06878
DJLauwy <laudehoop@hotmail.com>
2020-11-30 13:24:24 +0100

    video 7 afgerond

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ document.querySelector('.animeer-knop').addEventListener('click', (e) => {
 setTimeout(()=>{
 	document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
 }, 2000);
+
+//een klik op nav__knop laat de nav__link (alle) na elkaar naar binnen schuiven
+//door ze de class nav__link--schuif-in te geven
+const menuKnop = document.querySelector('.nav__knop');
+const navItems = document.querySelectorAll('.nav__link');
+menuKnop.addEventListener('click', () => {
+	navItems.forEach((item,index) => {
+		setTimeout(() => {
+			item.classList.toggle('nav__link--schuif-in');
+		}, 100 * index);
+	});
+	document.querySelector('.fa-bars').classList.toggle('verbergen');
+	document.querySelector('.fa-times').classList.toggle('verbergen');
+});
